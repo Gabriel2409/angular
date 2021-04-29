9c30afe055be49f0a81afb0e65a81ee7b3389a24
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-04-29 08:49:59 +0200

    added server component

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent} from './server/server.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, ServerComponent],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
