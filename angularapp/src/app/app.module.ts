f99308b54e42c68a56d4f377ed7d4e75d13d234c
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-04-30 17:50:38 +0200

    two way data binding

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ServerComponent} from './server/server.component';
@@ -9,7 +10,7 @@ import { ErrorAlertComponent } from './error-alert/error-alert.component'
 
 @NgModule({
     declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, ErrorAlertComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
