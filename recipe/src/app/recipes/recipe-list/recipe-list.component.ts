0f749ce68344a84ff20033b64a895cd6750da84b
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-05-06 08:58:35 +0200

    created a model and used it

--- recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/05/homemade-birria-tacos-recipe-3273w.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
