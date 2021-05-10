24f688442eccdb0f2d95c40211fdd6095df3ffbb
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-05-10 18:05:41 +0200

    Recipe : navigation with ngIF

--- recipe/src/app/app.component.ts
+++ recipe/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'recipe';
+  loadedFeature: string = 'recipe';
+
+  onNavigate(feature: string) {
+    if (feature !== this.loadedFeature) {
+      this.loadedFeature = feature;
+    }
+  }
 }
