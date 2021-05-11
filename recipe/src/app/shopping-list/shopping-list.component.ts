35d5e4d18f2359f9236a0566d62e193c29558c2a
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-05-11 10:31:20 +0200

    RECIPE : shopping list with viewchild

--- recipe/src/app/shopping-list/shopping-list.component.ts
+++ recipe/src/app/shopping-list/shopping-list.component.ts
@@ -14,4 +14,8 @@ export class ShoppingListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onIngredientAdded(ingredientData: Ingredient) {
+    this.ingredients.push(ingredientData);
+  }
 }
