1cda5c712f409d1a36ec0d0c6b1e0f86b537ec88
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-05-11 09:03:24 +0200

    RECIPE : forwarded changes by creating a cascade of events

--- recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -13,9 +13,20 @@ export class RecipeListComponent implements OnInit {
       'This is simply a test',
       'https://iamafoodblog.b-cdn.net/wp-content/uploads/2020/05/homemade-birria-tacos-recipe-3273w.jpg'
     ),
+    new Recipe(
+      'Another test recipe',
+      'Another test',
+      'https://www.simplyrecipes.com/thmb/O-rhPnz2V3hdqKFPij8NlwZIKqs=/2376x1584/filters:fill(auto,1)/Simply-Recipes-Quesadilla-LEAD-5-55da42a2a306497c85b1328385e44d85.jpg'
+    ),
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipeData: Recipe) {
+    this.recipeWasSelected.emit(recipeData);
+  }
 }
