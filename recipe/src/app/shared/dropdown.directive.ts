337063a9aa8fc366895b293d0149aadf2bd7a1fd
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-05-12 17:48:41 +0200

    RECIPE pretty code to close dropdown from anywhere

--- recipe/src/app/shared/dropdown.directive.ts
+++ recipe/src/app/shared/dropdown.directive.ts
@@ -11,10 +11,10 @@ import {
 })
 export class DropdownDirective implements OnInit {
   @HostBinding("class.open") isOpen = false;
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event:Event) {
+	this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
 
-  constructor() {}
+  constructor(private elRef: ElementRef) {}
   ngOnInit() {}
 }
