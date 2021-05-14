95fc934ff3169a2a40d103a382e390b381232642
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-05-14 15:26:41 +0200

    moved service to app.module

--- services/src/app/app.component.ts
+++ services/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { AccountsService } from "./accounts.service";
-import { AccountComponent } from "./account/account.component";
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  providers: [AccountsService],
 })
 export class AppComponent implements OnInit {
   accounts: { name: string; status: string }[] = [];
